68ae376da364ccaf97ed28d45d01149a9f2beff5
Martin Oemus <martin@oemus.net>
2013-12-09 10:09:07 +0100

    [Console] Farbsensor-View benutzbar

--- tools/TURAG-Console/Console/frontend/feldbusviews/feldbusfarbsensorview.cpp
+++ tools/TURAG-Console/Console/frontend/feldbusviews/feldbusfarbsensorview.cpp
@@ -1,12 +1,85 @@
 #include "feldbusfarbsensorview.h"
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QLabel>
+#include <frontend/util/datagraph.h>
+#include <QTimer>
+#include <QTime>
+#include <QDebug>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGridLayout>
+
+using namespace TURAG::Feldbus;
+
 
 FeldbusFarbsensorView::FeldbusFarbsensorView(Farbsensor *farbsensor, QWidget *parent) :
     QWidget(parent), farbsensor_(farbsensor)
 {
-    QHBoxLayout* hlayout = new QHBoxLayout;
-    QLabel* title = new QLabel("Farbsensor-Analyse");
-    hlayout->addWidget(title);
-    setLayout(hlayout);
+    QGridLayout* layout = new QGridLayout;
+    QLabel* title = new QLabel("<br>Farbsensor-Analyse</br>");
+    layout->addWidget(title, 0, 0, 1, 2);
+
+    rgbGraph = new DataGraph;
+    hsvGraph = new DataGraph;
+    layout->addWidget(rgbGraph, 1, 0);
+    layout->addWidget(hsvGraph, 1, 1);
+
+    rgbScene = new QGraphicsScene(QRectF(0 ,0, 150, 150), this);
+    QGraphicsView* rgbView = new QGraphicsView(rgbScene);
+    layout->addWidget(rgbView, 2, 0);
+
+    hsvScene = new QGraphicsScene(QRectF(0 ,0, 150, 150), this);
+    QGraphicsView* hsvView = new QGraphicsView(hsvScene);
+    layout->addWidget(hsvView, 2, 1);
+
+
+
+    setLayout(layout);
+
+    rgbGraph->addChannel("R", (qreal)5, (qreal)0, (qreal)256);
+    rgbGraph->addChannel("G", (qreal)5, (qreal)0, (qreal)256);
+    rgbGraph->addChannel("B", (qreal)5, (qreal)0, (qreal)256);
+
+    hsvGraph->addChannel("H", (qreal)5, (qreal)0, (qreal)256);
+    hsvGraph->addChannel("S", (qreal)5, (qreal)0, (qreal)256);
+    hsvGraph->addChannel("V", (qreal)5, (qreal)0, (qreal)256);
+
+    time_ = new QTime();
+    time_->start();
+
+    mainInterval = new QTimer(this);
+    connect(mainInterval, SIGNAL(timeout()), this, SLOT(mainIntervalTimeout()));
+    mainInterval->start(100);
 }
+
+FeldbusFarbsensorView::~FeldbusFarbsensorView() {
+    delete time_;
+}
+
+void FeldbusFarbsensorView::mainIntervalTimeout() {
+    Farbsensor::rgb_t rgb;
+    if (farbsensor_->getRGB(&rgb)) {
+        qreal currentTime = static_cast<qreal>(time_->elapsed()) / 1000;
+
+        rgbGraph->addData(0, QPointF(currentTime, rgb.r));
+        rgbGraph->addData(1, QPointF(currentTime, rgb.g));
+        rgbGraph->addData(2, QPointF(currentTime, rgb.b));
+
+        rgbScene->setBackgroundBrush(QBrush(QColor(rgb.r, rgb.g, rgb.b)));
+    }
+
+    Farbsensor::hsv_t hsv;
+    if (farbsensor_->getHSV(&hsv)) {
+        qreal currentTime = static_cast<qreal>(time_->elapsed()) / 1000;
+
+        hsvGraph->addData(0, QPointF(currentTime, hsv.h));
+        hsvGraph->addData(1, QPointF(currentTime, hsv.s));
+        hsvGraph->addData(2, QPointF(currentTime, hsv.v));
+
+        QColor color;
+        color.setHsv(hsv.h / (6*42) * 360, hsv.s, hsv.v);
+        hsvScene->setBackgroundBrush(QBrush(color));
+    }
+}
+
--- tools/TURAG-Console/Console/frontend/feldbusviews/feldbusfarbsensorview.h
+++ tools/TURAG-Console/Console/frontend/feldbusviews/feldbusfarbsensorview.h
@@ -4,6 +4,11 @@
 #include <QWidget>
 #include <tina++/feldbus/host/farbsensor.h>
 
+class DataGraph;
+class QTimer;
+class QTime;
+class QGraphicsScene;
+
 using namespace TURAG::Feldbus;
 
 class FeldbusFarbsensorView : public QWidget
@@ -11,9 +16,20 @@ class FeldbusFarbsensorView : public QWidget
     Q_OBJECT
 protected:
     Farbsensor* farbsensor_;
+    DataGraph* rgbGraph;
+    DataGraph* hsvGraph;
+    QTimer* mainInterval;
+    QTime* time_;
+
+    QGraphicsScene* rgbScene;
+    QGraphicsScene* hsvScene;
+
+protected slots:
+    void mainIntervalTimeout();
 
 public:
     explicit FeldbusFarbsensorView(Farbsensor* farbsensor, QWidget *parent = 0);
+    ~FeldbusFarbsensorView();
 
 signals:
 
