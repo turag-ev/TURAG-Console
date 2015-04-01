e68e1261c329d757a70b6bfb561a906fd465da3b
Martin Oemus <martin@oemus.net>
2015-04-01 03:58:06 +0200

    [TC] Kosmetik

--- tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderatmegaview.cpp
+++ tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderatmegaview.cpp
@@ -46,9 +46,9 @@ FeldbusBootloaderAtmegaView::FeldbusBootloaderAtmegaView(TURAG::Feldbus::Bootloa
 		highFuseEdit->setText("Error");
 		extFuseEdit->setText("Error");
 	} else {
-		lowFuseEdit->setText(QString("0x%1").arg(static_cast<ulong>(fuses.low), 0, 16));
-		highFuseEdit->setText(QString("0x%1").arg(static_cast<ulong>(fuses.high), 0, 16));
-		extFuseEdit->setText(QString("0x%1").arg(static_cast<ulong>(fuses.extended), 0, 16));
+		lowFuseEdit->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.low), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.low), -8, 2, static_cast<QChar>('0')));
+		highFuseEdit->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.high), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.high), -8, 2, static_cast<QChar>('0')));
+		extFuseEdit->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.extended), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.extended), -8, 2, static_cast<QChar>('0')));
 	}
 }
 
--- tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderxmegaview.cpp
+++ tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderxmegaview.cpp
@@ -45,43 +45,43 @@ FeldbusBootloaderXmegaView::FeldbusBootloaderXmegaView(TURAG::Feldbus::Bootloade
 		if (fuses.fuse0.available) {
 			QLineEdit* fuse0 = new QLineEdit;
 			setDisabledTheme(fuse0);
-			fuse0->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse0.value), 0, 16));
+			fuse0->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse0.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse0.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 0:", fuse0);
 		}
 		if (fuses.fuse1.available) {
 			QLineEdit* fuse1 = new QLineEdit;
 			setDisabledTheme(fuse1);
-			fuse1->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse1.value), 0, 16));
+			fuse1->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse1.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse1.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 1:", fuse1);
 		}
 		if (fuses.fuse2.available) {
 			QLineEdit* fuse2 = new QLineEdit;
 			setDisabledTheme(fuse2);
-			fuse2->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse2.value), 0, 16));
+			fuse2->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse2.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse2.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 2:", fuse2);
 		}
 		if (fuses.fuse3.available) {
 			QLineEdit* fuse3 = new QLineEdit;
 			setDisabledTheme(fuse3);
-			fuse3->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse3.value), 0, 16));
+			fuse3->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse3.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse3.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 3:", fuse3);
 		}
 		if (fuses.fuse4.available) {
 			QLineEdit* fuse4 = new QLineEdit;
 			setDisabledTheme(fuse4);
-			fuse4->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse4.value), 0, 16));
+			fuse4->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse4.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse4.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 4:", fuse4);
 		}
 		if (fuses.fuse5.available) {
 			QLineEdit* fuse5 = new QLineEdit;
 			setDisabledTheme(fuse5);
-			fuse5->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse5.value), 0, 16));
+			fuse5->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse5.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse5.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 5:", fuse5);
 		}
 		if (fuses.fuse6.available) {
 			QLineEdit* fuse6 = new QLineEdit;
 			setDisabledTheme(fuse6);
-			fuse6->setText(QString("0x%1").arg(static_cast<ulong>(fuses.fuse6.value), 0, 16));
+			fuse6->setText(QString("0x%1\t0b%2").arg(static_cast<ulong>(fuses.fuse6.value), -2, 16, static_cast<QChar>('0')).arg(static_cast<ulong>(fuses.fuse6.value), -8, 2, static_cast<QChar>('0')));
 			infoLayout->addRow("Fuse 6:", fuse6);
 		}
 	}
