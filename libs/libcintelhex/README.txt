8a378764c1e550f21f533e39b83231c8231efb5d
Martin Oemus <martin@oemus.net>
2015-06-29 17:27:31 +0200

    [libs] libcintelhex hinzugefügt

---
+++ libs/libcintelhex/README.txt
@@ -0,0 +1,6 @@
+These files are forked from the following project:
+
+https://github.com/martin-helmich/libcintelhex
+
+and processed for being compiled into some project without
+any library troubles.
---
+++ libs/libcintelhex/config.h
@@ -0,0 +1,11 @@
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have a working `mmap' system call. */
+#define HAVE_MMAP 0
+
+/* Define to 1 if you have the <sys/mman.h> header file. */
+#define HAVE_SYS_MMAN_H 0
+
+
