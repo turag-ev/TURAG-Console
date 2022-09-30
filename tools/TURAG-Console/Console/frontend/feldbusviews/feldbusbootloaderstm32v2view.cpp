6a417f717163bd2097d7b1b4c7ba9a9be838a743
Martin Oemus <martin.oemus@ikts.fraunhofer.de>
2022-09-30 13:06:07 +0200

    Console: adapted stm32 bootloader view to protocol changes

--- tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderstm32v2view.cpp
+++ tools/TURAG-Console/Console/frontend/feldbusviews/feldbusbootloaderstm32v2view.cpp
@@ -7,44 +7,28 @@
 FeldbusBootloaderStm32v2View::FeldbusBootloaderStm32v2View(TURAG::Feldbus::BootloaderStm32v2 *bootloader, FeldbusFrontend *bus_, QWidget *parent) :
     FeldbusBootloaderBaseView(bootloader, bus_, parent), bootloader(bootloader)
 {
-    QLineEdit* bootloaderInitialStackAddress = new QLineEdit;
-    setDisabledTheme(bootloaderInitialStackAddress);
-    infoLayout->addRow("Bootloader initial stack address:", bootloaderInitialStackAddress);
-
-    QLineEdit* bootloaderResethandlerAddress = new QLineEdit;
-    setDisabledTheme(bootloaderResethandlerAddress);
-    infoLayout->addRow("Bootloader reset handler address:", bootloaderResethandlerAddress);
+    QLineEdit* resetHandlerStorageAddress = new QLineEdit;
+    setDisabledTheme(resetHandlerStorageAddress);
+    infoLayout->addRow("Bootloader reset handler address:", resetHandlerStorageAddress);
 
 	if (!bootloader || !bootloader->isUnlocked()) {
 		return;
 	}
 
-
-    // read stack address
-    uint32_t stackAddress = bootloader->getBootloaderInitialStackAddress();
-    if (stackAddress == 0xFFFFFFFF) {
-        bootloaderInitialStackAddress->setText("Error");
+    // read reset handler storage address
+    uint32_t storageAddress = bootloader->getResetVectorStorageAddress();
+    if (storageAddress == 0xFFFFFFFF) {
+        resetHandlerStorageAddress->setText("Error");
     } else {
-        bootloaderInitialStackAddress->setText(QString("0x%1").arg(stackAddress, 2, 16));
-    }
-
-    // reset handler address
-    uint32_t resetHandler = bootloader->getBootloaderResetHandlerAddress();
-    if (resetHandler == 0xFFFFFFFF) {
-        bootloaderResethandlerAddress->setText("Error");
-    } else {
-        bootloaderResethandlerAddress->setText(QString("0x%1").arg(resetHandler, 2, 16));
+        resetHandlerStorageAddress->setText(QString("0x%1").arg(storageAddress, 2, 16));
     }
 }
 
 
 TURAG::Feldbus::BootloaderAvrBase::ErrorCode FeldbusBootloaderStm32v2View::doFlashImage(uint8_t* data, uint32_t length) {
-    uint32_t stmFlashBaseAddress = 0x08000000;
+    uint32_t resetVectorStorageAddress = bootloader->getResetVectorStorageAddress();
 
-    uint32_t bootloaderStackAddress = bootloader->getBootloaderInitialStackAddress();
-    uint32_t bootloaderResetHandlerAddress = bootloader->getBootloaderResetHandlerAddress();
-
-    if (bootloaderStackAddress == 0xFFFFFFFF || bootloaderResetHandlerAddress == 0xFFFFFFFF) {
+    if (resetVectorStorageAddress == 0xFFFFFFFF) {
         return TURAG::Feldbus::BootloaderAvrBase::ErrorCode::invalid_args;
     }
 
@@ -54,22 +38,55 @@ TURAG::Feldbus::BootloaderAvrBase::ErrorCode FeldbusBootloaderStm32v2View::doFla
     memcpy(&userAppResetHandlerAddress, data + 4, 4);
 
     // send these addresses to the device
-    qDebug() << "writing app reset vectors\n";
-    auto result = bootloader->writeAppResetVectors(userAppStackAddress, userAppResetHandlerAddress);
+    qDebug() << "transmitting app reset vectors\n";
+    auto result = bootloader->transmitAppResetVectors(userAppStackAddress, userAppResetHandlerAddress);
     if (result != TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success) {
         return result;
     }
 
-    // replace app reset vectors with the addresses as reported by the bootloader
-    memcpy(data, &bootloaderStackAddress, 4);
-    memcpy(data + 4, &bootloaderResetHandlerAddress, 4);
-
-    // write modified image to device
+    // write image to device
     qDebug() << "writing " << length << " bytes to address " << stmFlashBaseAddress << "\n";
     result = bootloader->writeFlash(stmFlashBaseAddress, length, data);
     if (result != TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success) {
         return result;
     }
 
+    // commit user app addresses
+    qDebug() << "committing app reset vectors\n";
+    result = bootloader->commitAppResetVectors(userAppStackAddress, userAppResetHandlerAddress);
+    if (result != TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success) {
+        return result;
+    }
+
     return TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success;
 }
+
+TURAG::Feldbus::BootloaderAvrBase::ErrorCode FeldbusBootloaderStm32v2View::doVerifyImage(uint8_t* data, uint32_t length) {
+    // verify
+    qDebug() << "verifying flash content\n";
+    uint8_t readData[length];
+
+    auto result = bootloader->readFlash(stmFlashBaseAddress, length, readData);
+    if (result != TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success) {
+        return result;
+    }
+
+    // replace the first 8 byte because they are changed by the bootloader
+    memcpy(readData, data, 8);
+
+    // if the reset vector is stored someplace within the image we need to replace this data
+    // with the original, otherwise the verification would fail
+    uint32_t maskOffset = bootloader->getResetVectorStorageAddress() & ~stmFlashBaseAddress;
+    if (maskOffset + 8 <= length) {
+        memcpy(readData + maskOffset, data + maskOffset, 8);
+    }
+
+    for (uint32_t i = 0; i < length; ++i) {
+        if (data[i] != readData[i]) {
+            return TURAG::Feldbus::BootloaderAvrBase::ErrorCode::content_mismatch;
+        }
+    }
+
+    return TURAG::Feldbus::BootloaderAvrBase::ErrorCode::success;
+}
+
